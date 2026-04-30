53c4663de7620ec222459eedc962ee3fdb3def4e
fan <1210326244@qq.com>
2026-04-30 18:16:43 +0800

    修改端口

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from night_train_app.app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5500))
+    port = int(os.environ.get("PORT", 5000))
     print(f"🌙 夜行列车推荐已启动：http://127.0.0.1:{port}")
     app.run(host="127.0.0.1", port=port, debug=True)
